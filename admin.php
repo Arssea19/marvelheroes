5eb4d03fd1c13db5595fbdf8d4bf5dc48f8de830
Ars <22rayatha@gmail.com>
2026-01-14 21:12:39 +0700

    ubah admin.php

--- admin.php
+++ admin.php
@@ -54,12 +54,12 @@ if (!isset($_SESSION['username'])) {
             <?= $_SESSION['username']?>
           </a>
           <ul class="dropdown-menu">
+            <li><hr class="dropdown-divider"></li>
             <li>
               <a class="dropdown-item" href="profil.php">
                 <i class="bi bi-person"></i> Profile
               </a>
             </li>
-            <li><hr class="dropdown-divider"></li>
             <li>
               <a class="dropdown-item" href="logout.php">
                 <i class="bi bi-box-arrow-right"></i> Logout
