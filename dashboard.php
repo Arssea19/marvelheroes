49869bf06a6fbeb110a55f007ba21c3e6ab1655a
Ars <22rayatha@gmail.com>
2026-01-14 15:17:40 +0700

    ubah dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2,10 +2,19 @@
 //query untuk mengambil data article
 $sql1 = "SELECT * FROM article ORDER BY tanggal DESC";
 $hasil1 = $conn->query($sql1);
+$jumlah_article = $hasil1->num_rows;
 
-//menghitung jumlah baris data article
-$jumlah_article = $hasil1->num_rows; 
+//untuk menambah gallery
+$sql2 = "SELECT * FROM gallery";
+$hasil2 = $conn->query($sql2);
+$jumlah_gallery = $hasil2->num_rows;
 ?>
+<div class="text-center mb-4">
+    <h4>Selamat Datang,</h4>
+    <h2 class="fw-bold"><?= $_SESSION['username']; ?></h2>
+    <img src="uploads/<?= $_SESSION['foto']; ?>" 
+         class="rounded-circle mt-2" width="120">
+</div>
 <div class="row row-cols-1 row-cols-md-4 g-4 justify-content-center pt-4">
     <div class="col">
         <div class="card border border-success mb-3 shadow" style="max-width: 18rem;">
@@ -29,7 +38,9 @@ $jumlah_article = $hasil1->num_rows;
                         <h5 class="card-title"><i class="bi bi-camera"></i> Gallery</h5> 
                     </div>
                     <div class="p-3">
-                        <span class="badge rounded-pill text-bg-success fs-2">0</span>
+                        <span class="badge rounded-pill text-bg-success fs-2">
+                            <?php echo $jumlah_gallery; ?>
+                        </span>
                     </div> 
                 </div>
             </div>
